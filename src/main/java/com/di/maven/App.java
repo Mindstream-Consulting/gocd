6063c49f0ae9079d17e6abbde2a1e08db820ae15
bimalpandey <bimal@mindstream-consulting.com>
2015-03-24 13:56:10 -0700

    Demo to Tim #30

--- src/main/java/com/di/maven/App.java
+++ src/main/java/com/di/maven/App.java
@@ -9,9 +9,12 @@ public class App
     public static void main( String[] args )
 
     {
+        
         System.out.println( "Hello Mindstream!" );
         
+        
 	System.out.println( "Welcome!" );
+        
      
     }
 
